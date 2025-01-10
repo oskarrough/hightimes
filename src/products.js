f71513b010ba30249844751ea27108d17e2c95e0
Jboxtel <jeroen.boxtel@gmail.com>
2025-01-10 21:01:32 +0100

    updated products

--- src/products.js
+++ src/products.js
@@ -1,12 +1,42 @@
 export const products = [
 	{
-		name: 'apple',
-		quantity: 10,
-		price: 1
+		name: 'Cough Syrup',
+		quantity: 200,
+		price: 5
 	},
 	{
-		name: 'oranges',
+		name: 'Weed',
+		quantity: 150,
+		price: 15
+	}, 
+	{
+		name: 'Speed',
+		quantity: 50,
+		price: 25
+	}, 
+	{
+		name: 'LSD',
+		quantity: 30,
+		price: 50
+	}, 
+	{
+		name: 'Cocaine',
 		quantity: 20,
-		price: 3
+		price: 100
+	}, 
+	{
+		name: 'Meth',
+		quantity: 15,
+		price: 80
+	}, 
+	{
+		name: 'Crack',
+		quantity: 20,
+		price: 60
+	}, 
+	{
+		name: 'Heroin',
+		quantity: 10,
+		price: 120
 	}
-]
+];

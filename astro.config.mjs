935b1ddf0a8ac2f9af6d81e992a2983b965076b3
Oskar Rough <oskar@rough.dk>
2025-01-12 13:30:01 +0100

    Disable astro dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte()]
+	integrations: [svelte()],
+	devToolbar: {
+		enabled: false
+	}
 })
